156e041f7b7d6d5fe6d8d7084c544009002ebf55
e205721HarukiO <e205721@ie.u-ryukyu.ac.jp>
2022-09-27 18:00:29 +0900

    refactoring: コメントの追加など

--- src/movie_process.py
+++ src/movie_process.py
@@ -178,19 +178,26 @@ def movie_process(target_file, output_folder="result"):
     cap_file = cv2.VideoCapture(target_file)
     output_file = open(output_file_path, "w")
     output_file.write("動画時間, 試合, 連鎖数, 発火前ぷよ量, 発火後ぷよ量\n")
+
     print(f"動画読み込み成否->{cap_file.isOpened()}")
+
     kakeru_img = cv2.imread("./kakeru.png", 0)
     kakeru_img = cv2.resize(kakeru_img,(12,12))
     score_0000_img = cv2.imread("./score_0000.png", 0)
+
+    # 各変数の初期化
     next_flag = True
     prev_img = None
     match_started_flag = True
-    puyo_function = puyo_simulator.puyo_simulator()
-
-    match_count = 0
     frame_count = 0
+    match_count = 0
     num_puyo_before = 0
     num_puyo_after = 0
+
+    # ゲームシミュレータを格納.
+    puyo_function = puyo_simulator.puyo_simulator()
+
+    # 動画のfpsを取得
     video_fps = cap_file.get(cv2.CAP_PROP_FPS)
 
     #画像を1フレームずつ取得。終わったらループを抜ける
@@ -223,13 +230,14 @@ def movie_process(target_file, output_folder="result"):
         kakeru_flag = check_multiple_mark(img_1p, kakeru_img)
         if(kakeru_flag and next_flag):
             mask_imgs = make_mask_imgs(img_1p) #マスク画像を生成
-            # 1回だけ連鎖数を読み取る
-            puyo_function.field = make_field(mask_imgs)
+            # 連鎖数を読み取る
+            puyo_function.field = make_field(mask_imgs) # 盤面を配列の情報にする.
             num_puyo_before = puyo_function.count_puyo() # ぷよ量カウント
             num_chain = puyo_function.chain() # 連鎖数
             movie_time = calc_videotime(video_fps, frame_count) # 動画時間
             output_file.write(f"{movie_time}, {match_count}, {num_chain}, {num_puyo_before}, ") # 結果の書き込み
             next_flag = False
             # 次に前のフレームまでツモ欄が動く(next_flag=true)まではスルー.
-        prev_img = img_1p
+        prev_img = img_1p # 更新
+
     output_file.close()
